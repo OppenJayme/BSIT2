f6ed94636fe7d40aa19952aab477a8433440eef1
Kyer Ivan <156319128+OppenJayme@users.noreply.github.com>
2024-10-09 09:33:19 +0800

    Update example1.c

--- Prog2/activities/Activity6/example1.c
+++ Prog2/activities/Activity6/example1.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 typedef struct {
 char name[50]; 
@@ -19,12 +20,14 @@ for (int i = 0; i < size; i++)
 printf("Student %d: Name = %s, Age = %d\n", i + 1, students[i].name,
 students[i].age);
 }
+printf("\n");
 }
 
 void updateStudent(Student students[], int index, const char *newName, int newAge)
 {
 strcpy(students[index].name, newName); 
 students[index].age = newAge; 
+printf("updated successfully\n");
 }
 
 void deleteStudent(Student students[], int *size, int index)
@@ -34,6 +37,7 @@ for (int i = index; i < *size - 1; i++)
 students[i] = students[i + 1]; 
 }
 (*size)--; 
+printf("Deleted successfully\n");
 }
 
 
@@ -51,14 +55,14 @@ for (int i = 0; i < size; ++i){
     printf("Enter name of student %d: ", i+1);
     scanf("%s", students[i].name);
     printf("Enter age of student %d: ", i+1);
-    scanf("%s", students[i].age);
+    scanf("%d", &students[i].age);
     createStudent(students, i, students[i].name, students[i].age); 
 }
-printf("After Adding Students:\n");
+printf("\nAfter Adding Students:\n");
 readStudents(students, size);
 
 while(1){
-    printf("Student Management...\n");
+    printf("\nStudent Management...\n");
     printf("1. Update student by index.\n");
     printf("2. Delete Student by index.\n");
     printf("3. Read/Display students.\n");
@@ -72,25 +76,36 @@ while(1){
             int index;
             char newName[50];
             int newAge;
-        
+            
             printf("Enter index of student: ");
-            scanf("%d", &index);
+            if (scanf("%d", &index) > size){
+                printf("Index is invalid.\n");
+            };
             printf("Enter new name of student: ");
             scanf("%s", newName);
             printf("Enter new age: ");
             scanf("%d", &newAge);
+            printf("\n");
             updateStudent(students, index, newName, newAge); 
             break;
         case 2:
             int indexer;
             printf("Enter index of student to delete: ");
-            scanf("%d", &indexer);
+            if (scanf("%d", &indexer) > size){
+                printf("Index is invalid.\n");
+            };
             deleteStudent(students, &size, indexer); 
+            
+            break;
         case 3: 
             readStudents(students, size);
+            break;
+        case 4:
+            printf("Exiting...\n");
+            exit(1);
         default:
             printf("Invalid option");
-            break;
+            exit(1);
     }
 }
 
