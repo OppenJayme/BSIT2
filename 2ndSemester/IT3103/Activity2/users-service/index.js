8a6398904a50d32c90326ede9bbeb1dd82f194e4
OppenJayme <jaymekyerivan@gmail.com>
2025-03-12 16:36:16 +0800

    added activity2

---
+++ 2ndSemester/IT3103/Activity2/users-service/index.js
@@ -0,0 +1,44 @@
+const { ApolloServer, gql } = require("apollo-server");
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+const typeDefs = gql`
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+  }
+
+  type Query {
+    users: [User] 
+    user(id: ID!): User 
+  }
+
+  type Mutation {
+    createUser(name: String!, email: String!): User
+    updateUser(id: ID!, name: String, email: String): User
+    deleteUser(id: ID!): User
+  }
+`;
+
+
+const resolvers = {
+  Query: {
+    users: () => prisma.user.findMany(),
+    user: (_, { id }) => prisma.user.findUnique({ where: { id: Number(id) } }),
+  },
+  Mutation: {
+    createUser: (_, args) => prisma.user.create({ data: args }),
+    updateUser: (_, { id, ...args }) =>
+      prisma.user.update({ where: { id: Number(id) }, data: args }),
+    deleteUser: (_, { id }) =>
+      prisma.user.delete({ where: { id: Number(id) } }),
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen({ port: 4001 }).then(({ url }) => {
+  console.log(`🚀 Users service running at ${url}`);
+});
